2cd63d6b64d4bf58a79c0e99e2062ffecb83035e
anjalikrishna-11 <123520060+anjalikrishna-11@users.noreply.github.com>
2024-07-09 17:25:34 +0530

    Create Gallery.js

---
+++ src/components/Gallery.js
@@ -0,0 +1,21 @@
+// U35346496
+
+import React from 'react';
+import Profile from './Profile';
+
+const Gallery = ({ teamMembers }) => {
+  return (
+    <div className="gallery">
+      {teamMembers.map((member, index) => (
+        <Profile
+          key={index}
+          name={member.name}
+          role={member.role}
+          photo={member.photo}
+        />
+      ))}
+    </div>
+  );
+};
+
+export default Gallery;
